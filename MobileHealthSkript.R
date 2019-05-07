f69c8f0231358382aa7e025dde1f4a88603572ae
Franz Wahr <francois-p@hotmail.de>
2019-05-07 15:31:26 +0200

    codings documentation

--- MobileHealthSkript.R
+++ MobileHealthSkript.R
@@ -1,4 +1,13 @@
+#just some test commands
 plot(rnorm(1000))
 x <- rnorm(1000)
 hist(x,freq=F)
 curve(dnorm(x), add=T)
+
+# make surey data are placed in the right directory
+getwd()
+
+my.data <- read.table(header=TRUE, "Preprocessed Activity Log Data Export.txt", sep="|")
+summary(my.data)
+
+# TODO devide walking cand cyling data groups
