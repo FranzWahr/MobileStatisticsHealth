9b90cd7730e02df0ac3f34bc6491bf253c4146c4
Franz Wahr <francois-p@hotmail.de>
2019-05-09 14:25:18 +0200

    new read in functions pasted from slides

--- MobileHealthSkript.R
+++ MobileHealthSkript.R
@@ -1,3 +1,6 @@
+install.packages("readxl")
+library("readxl")
+
 #just some test commands
 plot(rnorm(1000))
 x <- rnorm(1000)
@@ -7,10 +10,31 @@ curve(dnorm(x), add=T)
 # make surey data are placed in the right directory
 getwd()
 
-my.data <- read.table(header=TRUE, "Preprocessed Activity Log Data Export.txt", sep="|")
-my.data
-summary(my.data)
-head(my.data)
-my.data$value
+# read in StecOunter data
+my.StepCounterData <- read.table("./DataGroupedBySensor/StepCounter.csv", sep=",")
+my.StepCounterData
+summary(my.StepCounterData)
+head(my.StepCounterData)
+plot(my.StepCounterData)
+
+# read in SteDetector data
+my.StepDetectorData <- read.table("./DataGroupedBySensor/Stepdetector.csv", sep=",")
+head(my.StepDetectorData)
+plot(my.StepDetectorData)
+
+#read in AccelerometerSensor
+my.AccellerationData <- read_excel("./DataGroupedBySensor/AccelerometerSensor.xlsx")
+head(my.AccellerationData)
+
+#normalize timestemp
+mt <- min(my.AccellerationData$timestamp)
+my.AccellerationData$timestamp <- my.AccellerationData$timestamp - mt
+head(my.AccellerationData)
+
+#TODO error!
+my.AccellerationData$mag = sqrt(my.AccellerationData$x^2 + my.AccellerationData$y^2 + my.AccellerationData$z^2)
+
+
+
 
 # TODO devide walking cand cyling data groups
