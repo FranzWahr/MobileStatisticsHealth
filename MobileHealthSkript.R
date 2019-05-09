9a30b891f09ad7fffd70b54060bc6d5420c8b3a6
Franz Wahr <francois-p@hotmail.de>
2019-05-09 18:15:18 +0200

    vector sum copmputed and plots according to slides

--- MobileHealthSkript.R
+++ MobileHealthSkript.R
@@ -10,19 +10,19 @@ curve(dnorm(x), add=T)
 # make surey data are placed in the right directory
 getwd()
 
-# read in StecOunter data
+# read in StepcOunter data
 my.StepCounterData <- read.table("./DataGroupedBySensor/StepCounter.csv", sep=",")
 my.StepCounterData
 summary(my.StepCounterData)
 head(my.StepCounterData)
 plot(my.StepCounterData)
 
-# read in SteDetector data
+# read in SteDetector data TODO xlsx
 my.StepDetectorData <- read.table("./DataGroupedBySensor/Stepdetector.csv", sep=",")
 head(my.StepDetectorData)
 plot(my.StepDetectorData)
 
-#read in AccelerometerSensor
+#read in AccelerometerSensor TODO xlsx
 my.AccellerationData <- read_excel("./DataGroupedBySensor/AccelerometerSensor.xlsx")
 head(my.AccellerationData)
 
@@ -31,10 +31,34 @@ mt <- min(my.AccellerationData$timestamp)
 my.AccellerationData$timestamp <- my.AccellerationData$timestamp - mt
 head(my.AccellerationData)
 
-#TODO error!
-my.AccellerationData$mag = sqrt(my.AccellerationData$x^2 + my.AccellerationData$y^2 + my.AccellerationData$z^2)
+#compute vecot sum of acceleration vecotrs
+my.AccellerationData$mag = sqrt(as.numeric(my.AccellerationData$x)^2 + as.numeric(my.AccellerationData$y)^2 + as.numeric(my.AccellerationData$z)^2)
+head(my.AccellerationData)
+
+#parse vector values to numerics
+my.AccellerationData$x = as.numeric(my.AccellerationData$x)
+my.AccellerationData$y = as.numeric(my.AccellerationData$y)
+my.AccellerationData$z = as.numeric(my.AccellerationData$z)
+
+#plot all
+plot(my.AccellerationData$timestamp/1000, my.AccellerationData$mag, type="l",xlab="Seconds", ylab="Acceleration Magnitude")
 
 
 
+#compute mean and variance for both conditions
+mean(my.AccellerationData[my.AccellerationData$StatusID==21,]$mag)
+mean(my.AccellerationData[my.AccellerationData$StatusID==22,]$mag)
+var(my.AccellerationData[my.AccellerationData$StatusID==21,]$mag)
+var(my.AccellerationData[my.AccellerationData$StatusID==22,]$mag)
 
-# TODO devide walking cand cyling data groups
+#meanfor vertical acceleration (z)
+mean(my.AccellerationData[my.AccellerationData$StatusID==20,]$z)
+mean(my.AccellerationData[my.AccellerationData$StatusID==21,]$z)
+mean(my.AccellerationData[my.AccellerationData$StatusID==22,]$z)
+mean(my.AccellerationData[my.AccellerationData$StatusID==23,]$z)
+# working hypothesis derivable: vertical acceleration is lower in cycling
+
+
+# TODO devide and group walking cand cyling data groups
+
+
